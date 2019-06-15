1379b0d8d49d6b882d15d3cafc6d127d49444521
4brother <1916969309@qq.com>
2019-06-15 19:53:34 +0800

    密码使用MD5加密，登录失败提醒

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var utility = require('utility');
 let db = require('../model/db');
+
 /* GET home page. */
 router.get('/',function(req,res,next){
   res.render('login')
@@ -8,7 +10,7 @@ router.get('/',function(req,res,next){
 router.post('/submit',function(req,res,next){
     console.log(req.body)
     let name = req.body.name;
-    let password = req.body.password;
+    let password = utility.md5(req.body.password);
     let sql = `SELECT * FROM admin WHERE name=${name} AND password="${password}"`;
     console.log(sql)
     let connection = db.connection();
@@ -16,8 +18,13 @@ router.post('/submit',function(req,res,next){
       if(result.length > 0){
         req.session.ID = result[0].ID;
         req.session.name = result[0].name;
-      }
         res.redirect('../index/home');
+      }else {
+        res.send(`<script>
+                    alert('登录失败，请重新登录');
+                    location.href="../login";
+                  </script>`);
+      }
     });
     db.close(connection);
 });
